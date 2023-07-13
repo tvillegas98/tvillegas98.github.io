c5c340bff65efc80153e8ee7f8592782dd247878
tvillegas98 <tvillegas@fi.uba.ar>
2023-07-13 20:20:21 -0300

    Remove everything

--- js/helpers.js
+++
@@ -1,8 +0,0 @@
-function loader(){
-    document.write(`<div class="loader-wrapper">
-    <span class="loader"></span></span>
-    </div>`)
-    $(window).on("load",function(){
-    $(".loader-wrapper").fadeOut("slow");
-    });
-}
